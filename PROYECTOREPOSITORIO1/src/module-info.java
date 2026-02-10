99fe2f6832c2093ef00c22493f2e06eea4d15435
pabloveramsanjose <pabloveram.sanjose@fundacionloyola.net>
2026-02-10 09:09:15 +0100

    adri poke marica

---
+++ PROYECTOREPOSITORIO1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PROYECTOREPOSITORIO1 {
+}
---
+++ PROYECTOREPOSITORIO1/src/repositorio/main.java
@@ -0,0 +1,10 @@
+package repositorio;
+
+public class main {
+	public static void main(String[] args) {
+		
+	}
+		
+	}
+
+
